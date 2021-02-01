ccdb5966a99f5fed7ef84ddec1a418cf3325eee2
mytabworks <mytabworks@gmail.com>
2021-02-01 16:00:47 +0800

    💄 add main style

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './app';
 import reportWebVitals from './reportWebVitals';
+import "@styles/index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
