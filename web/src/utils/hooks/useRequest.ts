a338849504d11b8d6c13806a6c3cbdb1949f3ac2
mytabworks <mytabworks@gmail.com>
2021-02-02 17:46:59 +0800

    🔨add withCredentials to get the cookies set by server

--- web/src/utils/hooks/useRequest.ts
+++ web/src/utils/hooks/useRequest.ts
@@ -2,5 +2,8 @@ import { useAxios } from './useAxios'
 import { AxiosRequestConfig } from 'axios'
 
 export const useRequest = (endpoint: string, options?: AxiosRequestConfig) => {
-    return useAxios(process.env.REACT_APP_API_BASE_URL!, endpoint, options)
+    return useAxios(process.env.REACT_APP_API_BASE_URL!, endpoint, {
+        withCredentials: true, 
+        ...options
+    })
 }
