2411375d310b6df6f186aab9f19616dbb4f85082
mytabworks <mytabworks@gmail.com>
2021-02-02 17:54:03 +0800

    ✨created a hook that recieve the new access token

---
+++ web/src/utils/hooks/useRecieveToken.ts
@@ -0,0 +1,28 @@
+import { useEffect } from "react"
+import { SessionActionType, useSession } from "@components/Session"
+import { useAuthRequest } from "./useAuthRequest"
+
+export const useRefreshToken = () => {
+    const [, dispatch] = useSession()
+    const requestToken = useAuthRequest("/refresh_token", {
+        method: "POST"
+    })
+    useEffect(() => {
+        const callback = () => {
+            requestToken.call().then(response => {
+                if(!(response instanceof Error)) {
+                    dispatch({ 
+                        type: SessionActionType.SET_TOKEN,
+                        payload: response.data.accessToken
+                    })
+                }
+            })
+        }
+        callback()
+        const cleanup = setInterval(callback, 60000 * 15)
+
+        return () => {
+            clearInterval(cleanup)
+        }
+    }, [])
+}
