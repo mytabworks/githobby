03f11bc163a6e96989e4a804b0680c128b102f89
mytabworks <mytabworks@gmail.com>
2021-02-02 09:55:03 +0800

    🏗move the logic to useAxios for reusability

--- web/src/app/Pages/Landing/index.tsx
+++ web/src/app/Pages/Landing/index.tsx
@@ -1,23 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Row from '@components/Layout/Row'
 import Col from '@components/Layout/Col'
+import Container from '@components/Layout/Container'
 import Form from '@components/Form'
 import Button from '@components/Button'
 import Card from '@components/Card'
 import { Variant } from '@components/types'
+import { useGithub } from '@utils/hooks/useGithub'
+import Loader from '../../Loader'
 
 interface LandingProps {
     
 }
 
-const Landing: React.FunctionComponent<LandingProps> = (props) => {
+const Landing: React.FunctionComponent<LandingProps> = () => {
+    const [search, setSearch] = useState<string>('')
+    const request = useGithub(search ? '/search/repositories' : '/repositories')
+
+    useEffect(() => {
+        request.call({
+            params: { q: search, per_page: 100 }
+        })
+    }, [search])
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const formdata = new FormData(event.target as HTMLFormElement)
+        const search = formdata.get("search") as string
+        setSearch(search)
+    }
     return (
         <div className="pt-5">
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.InputGroup>
-                    <Form.Control placeholder="search repository"/>
+                    <Form.Control name="search" defaultValue={search} placeholder="search repository"/>
                     <Form.InputGroup.Append>
-                        <Button className="px-5" variant={Variant.Secondary}>SUBMIT</Button>
+                        <Button className="px-5" variant={Variant.Secondary} loading={request.loading} type="submit">SUBMIT</Button>
                     </Form.InputGroup.Append>
                 </Form.InputGroup>
             </Form>
@@ -26,7 +44,16 @@ const Landing: React.FunctionComponent<LandingProps> = (props) => {
                     <h3>repository</h3>
                 </Card.Header>
                 <Card.Body>
-                    sample body
+                    <Container className="py-3">
+                        {(search ? request.value?.data.items : request.value?.data)?.map((item: any) => {
+                            return (
+                                <Row key={item.id}>
+                                    <Col><a href={item.svn_url}>{item?.name}</a></Col>
+                                </Row>
+                            )
+                        })}
+                    </Container>
+                    {request.loading && <Loader />}
                 </Card.Body>
             </Card>
         </div>
--- web/src/utils/hooks/useRequest.ts
+++ web/src/utils/hooks/useRequest.ts
@@ -1,54 +1,6 @@
-import { useState } from "react";
-import axios, { AxiosRequestConfig } from "axios";
+import { useAxios } from './useAxios'
+import { AxiosRequestConfig } from 'axios'
 
-interface UseRequestStates<P> {
-    initial: boolean;
-    loading: boolean;
-    complete: boolean;
-    error: string | null;
-    value: P | null
-}
-
-export const useRequest = <P = any>(endpoint: string, options: AxiosRequestConfig = { method: "GET" }) => {
-    const [states, setStates] = useState<UseRequestStates<P>>({
-        initial: true,
-        loading: true,
-        complete: false,
-        error: null,
-        value: null
-    })
-    return {
-        ...states,
-        call(overideOptions?: AxiosRequestConfig) {
-            setStates((prev) => ({
-                ...prev, 
-                loading: true, 
-                complete: false, 
-            }))
-            return axios({
-                url: `${process.env.REACT_APP_API_BASE_URL}${endpoint}`,
-                ...options,
-                ...overideOptions
-            })
-            .then((response: any) => {
-                setStates((prev) => ({
-                    ...prev, 
-                    initial: false,
-                    loading: false, 
-                    complete: true, 
-                    value: response
-                }))
-                return response
-            })
-            .catch((error: Error) => {
-                setStates((prev) => ({
-                    ...prev, 
-                    initial: false,
-                    loading: false, 
-                    complete: true, 
-                    error: error.message
-                }))
-            })
-        }
-    }
+export const useRequest = (endpoint: string, options?: AxiosRequestConfig) => {
+    return useAxios(process.env.REACT_APP_API_BASE_URL!, endpoint, options)
 }
