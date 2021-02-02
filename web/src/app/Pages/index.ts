e1621cb90321f28e8edd8c237f621299d3f900f0
mytabworks <mytabworks@gmail.com>
2021-02-02 09:52:42 +0800

    ✨export login and register

--- web/src/app/Pages/index.ts
+++ web/src/app/Pages/index.ts
@@ -1,3 +1,7 @@
 import { lazy } from 'react'
 
 export const Landing = lazy(() => import('./Landing'))
+
+export const Login = lazy(() => import('./Login'))
+
+export const Registration = lazy(() => import('./Registration'))
