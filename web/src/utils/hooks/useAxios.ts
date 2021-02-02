91b59ec1fb59cde9ff90dce98c00d55640f46a94
mytabworks <mytabworks@gmail.com>
2021-02-02 09:55:58 +0800

    🚚move the useRequest logic here

---
+++ web/src/utils/hooks/useAxios.ts
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import axios, { AxiosRequestConfig } from "axios";
+
+export interface UseAxiosStates<P> {
+    initial: boolean;
+    loading: boolean;
+    complete: boolean;
+    error: string | null;
+    value: P | null
+}
+
+export const useAxios = <P = any>(baseurl: string, endpoint: string, options: AxiosRequestConfig = { method: "GET" }) => {
+    const [states, setStates] = useState<UseAxiosStates<P>>({
+        initial: true,
+        loading: true,
+        complete: false,
+        error: null,
+        value: null
+    })
+    return {
+        ...states,
+        call(overideOptions?: AxiosRequestConfig) {
+            setStates((prev) => ({
+                ...prev, 
+                loading: true, 
+                complete: false, 
+            }))
+            return axios({
+                url: `${baseurl}${endpoint}`,
+                ...options,
+                ...overideOptions
+            })
+            .then((response: any) => {
+                setStates((prev) => ({
+                    ...prev, 
+                    initial: false,
+                    loading: false, 
+                    complete: true, 
+                    value: response
+                }))
+                return response
+            })
+            .catch((error: Error) => {
+                setStates((prev) => ({
+                    ...prev, 
+                    initial: false,
+                    loading: false, 
+                    complete: true, 
+                    error: error.message
+                }))
+            })
+        }
+    }
+}
