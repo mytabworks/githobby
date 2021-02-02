96977c75398e2538073b15e4742ef980e482b95c
mytabworks <mytabworks@gmail.com>
2021-02-02 17:51:55 +0800

    ✨export basic profile and not found pages

--- web/src/app/Pages/index.ts
+++ web/src/app/Pages/index.ts
@@ -5,3 +5,7 @@ export const Landing = lazy(() => import('./Landing'))
 export const Login = lazy(() => import('./Login'))
 
 export const Registration = lazy(() => import('./Registration'))
+
+export const Profile = lazy(() => import('./Profile'))
+
+export const NotFound = lazy(() => import('./NotFound'))
