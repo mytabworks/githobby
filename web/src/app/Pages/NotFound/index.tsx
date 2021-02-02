6591e2aa922f707455707d5b11e4c4f4ff5fd4be
mytabworks <mytabworks@gmail.com>
2021-02-02 17:48:55 +0800

    ✨created a basic page not found

---
+++ web/src/app/Pages/NotFound/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const index: React.FunctionComponent = () => {
+    return (
+        <div className="d-flex flex-center">
+            <div className="mt-5">
+                <h1 className="t-center">404</h1>
+                <h2>Page Not Found</h2>
+            </div>
+        </div>
+    )
+}
+
+export default index
