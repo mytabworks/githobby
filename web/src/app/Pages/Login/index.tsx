c801d420216f621aaba72cea28e3815435a1f0dc
mytabworks <mytabworks@gmail.com>
2021-02-02 17:50:36 +0800

    🔨integrated login to server and send token to session

--- web/src/app/Pages/Login/index.tsx
+++ web/src/app/Pages/Login/index.tsx
@@ -1,35 +1,54 @@
 import React from 'react'
 import { FormEventType, FormProvider } from 'formydable'
+import { useHistory } from 'react-router-dom'
 import Row from '@components/Layout/Row'
 import Col from '@components/Layout/Col'
 import Input from '@components/FormValidated/Input'
 import Button from '@components/Button'
 import Card from '@components/Card'
+import Alert from '@components/Alert'
+import { SessionActionType, useSession } from '@components/Session'
 import { Variant } from '@components/types'
+import { useRequest } from '@utils/hooks/useRequest'
+import ServerErrorHandler from './ServerErrorHandler'
 
-interface LoginProps {
-    
-}
-
-const Login: React.FunctionComponent<LoginProps> = () => {
+const Login: React.FunctionComponent = () => {
+    const [, dispatch] = useSession()
+    const request = useRequest('/login', { method: "post" })
+    const history = useHistory<any>()
 
     const handleSubmit = (event: FormEventType) => {
-
+        if(event.isReady()) {
+            request.call({
+                data: event.json()
+            }).then(response => {
+                if(response.data?.status === "ok") {
+                    dispatch({ 
+                        type: SessionActionType.SET_TOKEN,
+                        payload: response.data.accessToken
+                    })
+                }
+            })
+        }
     }
-
+    
     return (
         <div className="pt-5">
             <Row>
-                <Col sm={{ offset: 1, span: 10}} md={{ offset: 4, span: 6}}>
+                <Col sm={{ offset: 1, span: 10}} lg={{ offset: 3, span: 6}}>
                     <Card className="mt-4" variant={Variant.Primary}>
                         <Card.Header>
-                            <h3>login</h3>
+                            <h3 className="mb-0">login</h3>
                         </Card.Header>
                         <Card.Body className="p-5">
                             <FormProvider onSubmit={handleSubmit}>
-                                <Input name="email" label="E-mail" rules="required|email" placeholder="enter your email..."  />
-                                <Input name="password" label="Password" rules="required" placeholder="enter your password..." type="password"  />
-                                <Button block variant={Variant.Secondary} type="submit">SUBMIT</Button>
+                                <Alert in={!!history.location.state?.success} variant={Variant.Success}>
+                                    {history.location?.state?.success}
+                                </Alert>
+                                <ServerErrorHandler request={request} />
+                                <Input name="email" label="E-mail" rules="required|email" placeholder="enter your email..." disabled={request.loading}/>
+                                <Input name="password" label="Password" rules="required" placeholder="enter your password..." type="password" disabled={request.loading}/>
+                                <Button block variant={Variant.Secondary} type="submit" className="mt-5" loading={request.loading}>SUBMIT</Button>
                             </FormProvider>
                         </Card.Body>
                     </Card>
