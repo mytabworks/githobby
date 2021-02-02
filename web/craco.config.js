3791749b9641bf89f16f6c2257296c27fec82119
mytabworks <mytabworks@gmail.com>
2021-02-02 17:35:54 +0800

    ✨add app source

--- web/craco.config.js
+++ web/craco.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 module.exports = {
   webpack: {
     alias: {
+      '@app': path.resolve(__dirname, "src/app/"),
       '@components': path.resolve(__dirname, "src/components/"),
       '@styles': path.resolve(__dirname, "src/assets/styles/"),
       '@images': path.resolve(__dirname, "src/assets/images/"),
