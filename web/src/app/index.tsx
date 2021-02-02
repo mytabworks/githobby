d60c4e5dd9f3461c1ae3d5b855dd56d3bb13c984
mytabworks <mytabworks@gmail.com>
2021-02-02 18:04:09 +0800

    ⚡️applying all the receiver and middleware routes

--- web/src/app/index.tsx
+++ web/src/app/index.tsx
@@ -1,13 +1,19 @@
 import React, { FunctionComponent } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { UserRole, useSession } from '@components/Session'
 import Container from '@components/Layout/Container'
+import ConditionalRoute from '@components/ConditinalRoute'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import Loader from './Loader'
-import { Landing, Login, Registration } from './Pages'
+import { Landing, Login, NotFound, Profile, Registration } from './Pages'
+import { useRecieveUser } from '@utils/hooks/useRecieveUser'
+import { useRefreshToken } from '@utils/hooks/useRecieveToken'
 
 export const App: FunctionComponent = () => {
-
+    const [{user}] = useSession()
+    useRefreshToken()
+    useRecieveUser()
     return (
         <BrowserRouter>
             <Navbar />
@@ -15,8 +21,16 @@ export const App: FunctionComponent = () => {
                 <React.Suspense fallback={<Loader />}>
                     <Switch>
                         <Route exact path="/" component={Landing}/>
-                        <Route path="/login" component={Login}/>
-                        <Route path="/register" component={Registration}/>
+                        <ConditionalRoute condition={!user} redirectTo="/" path="/login">
+                            <Login/>
+                        </ConditionalRoute>
+                        <ConditionalRoute condition={!user} redirectTo="/" path="/register">
+                            <Registration/>
+                        </ConditionalRoute>
+                        <ConditionalRoute condition={!!user && user.roles.includes(UserRole.CLIENT)} redirectTo="/login" path="/profile">
+                            <Profile/>
+                        </ConditionalRoute>
+                        <Route path="*" component={NotFound}/>
                     </Switch>
                 </React.Suspense>
             </Container>
