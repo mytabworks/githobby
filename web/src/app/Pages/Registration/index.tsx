ab5a6dbe764b3da2bfd36beefa2ad2f619313e0e
mytabworks <mytabworks@gmail.com>
2021-02-02 12:47:08 +0800

    ✨apply server api integration

--- web/src/app/Pages/Registration/index.tsx
+++ web/src/app/Pages/Registration/index.tsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FormEventType, FormProvider } from 'formydable'
+import { useHistory } from 'react-router-dom'
 import Row from '@components/Layout/Row'
 import Col from '@components/Layout/Col'
 import Input from '@components/FormValidated/Input'
 import Button from '@components/Button'
 import Card from '@components/Card'
 import { Variant } from '@components/types'
+import { useRequest } from '@utils/hooks/useRequest'
+import ServerErrorHandler from './ServerErrorHandler'
 
-interface RegisterProps {
+const Register: React.FunctionComponent = () => {
+    const request = useRequest('/registration', { method: "post" })
+    const history = useHistory()
     
-}
-
-const Register: React.FunctionComponent<RegisterProps> = () => {
+    useEffect(() => {
+        if(request.value?.status === "ok") {
+            history.push('/login', { 
+                success: "Successfully registered an account. you can login now." 
+            })
+        }
+    }, [request.value])
 
     const handleSubmit = (event: FormEventType) => {
-
+        if(event.isReady()) {
+            request.call({
+                data: event.json()
+            })
+        }
     }
-
+    
     return (
         <div className="pt-5">
             <Row>
@@ -27,11 +40,12 @@ const Register: React.FunctionComponent<RegisterProps> = () => {
                         </Card.Header>
                         <Card.Body className="p-5">
                             <FormProvider onSubmit={handleSubmit}>
-                                <Input name="username" label="Username" rules="required" placeholder="enter your github username..."  />
-                                <Input name="email" label="E-mail" rules="required|email" placeholder="enter your email..."  />
-                                <Input name="password" label="Password" rules="required|strong_password|min:8" placeholder="enter your password..." type="password"  />
-                                <Input name="confirm" label="Confirm Password" rules="required|same:password@Password" placeholder="confrim your password..." type="password"  />
-                                <Button block variant={Variant.Secondary} type="submit" className="mt-5">SUBMIT</Button>
+                                <ServerErrorHandler request={request} />
+                                <Input name="name" label="Github Username" rules="required" placeholder="enter your github username..." disabled={request.loading} />
+                                <Input name="email" label="E-mail" rules="required|email" placeholder="enter your email..." disabled={request.loading} />
+                                <Input name="password" label="Password" rules="required|strong_password|min:8" placeholder="enter your password..." type="password" disabled={request.loading} />
+                                <Input name="confirm" label="Confirm Password" rules="required|same:password@Password" placeholder="confrim your password..." type="password" disabled={request.loading} />
+                                <Button block variant={Variant.Secondary} type="submit" className="mt-5" loading={request.loading}>SUBMIT</Button>
                             </FormProvider>
                         </Card.Body>
                     </Card>
