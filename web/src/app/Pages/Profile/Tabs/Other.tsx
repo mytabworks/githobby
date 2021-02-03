5057ca2e6c70e6140e06d2e82baee834db920245
mytabworks <mytabworks@gmail.com>
2021-02-03 17:36:40 +0800

    ✨ created other tabs for github tab requests

---
+++ web/src/app/Pages/Profile/Tabs/Other.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Template from './Template'
+import { useGithub } from '@utils/hooks/useGithub'
+import Loader from '@app/Loader'
+import Repository from '@app/Pages/Landing/Repository'
+import Text from '@components/Text'
+
+interface OtherProps {
+    title: string;
+    fallback: React.ReactNode;
+    request: ReturnType<typeof useGithub>
+}
+
+const Other: React.FunctionComponent<OtherProps> = ({ title, request, fallback }) => {
+    return (
+        <Template icon="star-full" title={title}>
+            {request.value && request.value.length ? (
+                    request.value?.map((item: any) => {
+                        return (
+                            <Repository key={item.name} item={item} owner />
+                        )
+                    })
+                ) : (
+                    fallback
+                )
+            }
+            {request.loading && <Loader/>}
+        </Template>
+    )
+}
+
+export default Other
