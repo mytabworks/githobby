eada0cd869dbfc2344bfc85794960a37c76f2b65
mytabworks <mytabworks@gmail.com>
2021-02-03 10:17:22 +0800

    ✨integration for logout api

--- web/src/app/Navbar/index.tsx
+++ web/src/app/Navbar/index.tsx
@@ -2,13 +2,29 @@ import React from 'react'
 import Container from '@components/Layout/Container'
 import Nav from '@components/Nav'
 import Spinner from '@components/Spinner'
-import { useSession } from '@components/Session'
+import { SessionActionType, useSession } from '@components/Session'
 import { Link } from 'react-router-dom'
+import { useAuthRequest } from '@utils/hooks/useAuthRequest'
 import icon from '@images/icon.png'
 import './index.scss'
+import { AxiosResponse } from 'axios'
 
 const Navbar: React.FunctionComponent = () => {
-    const [{loading, user}] = useSession()
+    const [{loading, user}, dispatch] = useSession()
+    const requestLogout = useAuthRequest('/revoke_token', {
+        method: 'POST'
+    })
+    const handleLogout = (event: React.MouseEvent<HTMLAnchorElement>) => {
+        event.preventDefault()
+        requestLogout.call().then((response: AxiosResponse) => {
+            if(response.data.status === 'ok') {
+                dispatch({
+                    type: SessionActionType.SET_TOKEN,
+                    payload: ""
+                })
+            }
+        })
+    }
     return (
         <div className="navbar fixed-top">
             <Container>
@@ -23,7 +39,10 @@ const Navbar: React.FunctionComponent = () => {
                                 <Nav.Link to="/login" >Login</Nav.Link>
                             </>
                         ): (
-                            <Nav.Link to="/profile" >{user.name}</Nav.Link>
+                            <>
+                                <Nav.Link to="/profile" >{user.name}</Nav.Link>
+                                <Nav.Link to="/logout" as="a" href="/logout" onClick={handleLogout}>logout</Nav.Link>
+                            </>
                         )
                     )}
                 </Nav>
