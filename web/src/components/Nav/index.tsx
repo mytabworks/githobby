d02aec395f7c8935ce28c6da241063da456097cd
mytabworks <mytabworks@gmail.com>
2021-02-03 10:21:30 +0800

    ✨add tabs and column props

--- web/src/components/Nav/index.tsx
+++ web/src/components/Nav/index.tsx
@@ -2,14 +2,20 @@ import React, { FunctionComponent } from 'react'
 import { ComponentProps } from '../types'
 import Link from './Link'
 import './index.scss'
+import classNames from '@utils/classNames'
+
+interface NavProps extends ComponentProps {
+    column?: boolean;
+    tabs?: boolean;
+}
 
 interface NavComponent<P> extends FunctionComponent<P> {
     Link: typeof Link;
 }
 
-const Nav: NavComponent<ComponentProps> = ({ className, children, ...props }) => {
+const Nav: NavComponent<NavProps> = ({ column, tabs, className, children, ...props }) => {
     return (
-        <nav className={`nav${ className ? ` ${className}` : ''}`} {...props}>
+        <nav className={classNames('nav', { 'nav-column': column, 'nav-tabs': tabs }, className)} {...props}>
             {children}
         </nav>
     )
