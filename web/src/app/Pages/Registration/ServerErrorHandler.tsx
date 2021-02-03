51c74be9acf904f83432b26173274a196dbf87c4
mytabworks <mytabworks@gmail.com>
2021-02-03 10:19:45 +0800

    ✨add prompt when form are dirty and github username validation

--- web/src/app/Pages/Registration/ServerErrorHandler.tsx
+++ web/src/app/Pages/Registration/ServerErrorHandler.tsx
@@ -3,19 +3,24 @@ import { useFormField } from 'formydable'
 import { useRequest } from '@utils/hooks/useRequest'
 import Alert from '@components/Alert'
 import { Variant } from '@components/types'
+import { Prompt } from 'react-router-dom'
 interface ServerErrorHandlerProps {
     request: ReturnType<typeof useRequest>
 }
 
-const expectedMessages = ["E-mail was already taken"]
+const expectedMessages = ["E-mail was already taken", "Username doesn't exist on github"]
 
 const ServerErrorHandler: React.FunctionComponent<ServerErrorHandlerProps> = ({request}) => {
-    const { setFieldError } = useFormField()
-
+    const { setFieldError, dirty } = useFormField()
+    
     useEffect(() => {
         if(request.value?.status === "error" && expectedMessages.includes(request.value?.message)) {
             if(request.value?.message === expectedMessages[0]) {
                 setFieldError("email", request.value?.message)
+            }
+
+            if(request.value?.message === expectedMessages[1]) {
+                setFieldError("name", request.value?.message)
             }
         }
     }, [request.value])
@@ -25,9 +30,15 @@ const ServerErrorHandler: React.FunctionComponent<ServerErrorHandlerProps> = ({r
     !!request.error
 
     return (
-        <Alert variant={Variant.Secondary} in={showError}>
-            {request.error || request.value?.message}
-        </Alert>
+        <>
+            <Prompt
+                when={dirty}
+                message="Are you sure you want to leave?"
+                />
+            <Alert variant={Variant.Secondary} in={showError}>
+                {request.error || request.value?.message}
+            </Alert>
+        </>
     )
 }
 
