d030e13b7395fd0350b3bb2b1bce77e52852c970
mytabworks <mytabworks@gmail.com>
2021-02-03 17:39:41 +0800

    ✨ export all the lazy tabs

---
+++ web/src/app/Pages/Profile/Tabs/index.ts
@@ -0,0 +1,7 @@
+import { lazy } from 'react'
+
+export const Settings = lazy(() => import('./Settings'))
+
+export const Activities = lazy(() => import('./Activities'))
+
+export const Other = lazy(() => import('./Other'))
