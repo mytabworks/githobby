ad77d8f6f3198a89d3b5cc02b22f1acc25a920e2
mytabworks <mytabworks@gmail.com>
2021-02-03 17:48:25 +0800

    🎨 re-design fonts

--- web/src/app/Pages/NotFound/index.tsx
+++ web/src/app/Pages/NotFound/index.tsx
@@ -4,8 +4,8 @@ const index: React.FunctionComponent = () => {
     return (
         <div className="d-flex flex-center">
             <div className="mt-5">
-                <h1 className="t-center">404</h1>
-                <h2>Page Not Found</h2>
+                <h1 className="t-size-50 t-center">404</h1>
+                <h2 className="t-size-25">Page Not Found</h2>
             </div>
         </div>
     )
