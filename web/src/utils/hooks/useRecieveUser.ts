481886624a81a06550cb005d0f52d54ad6fc6201
mytabworks <mytabworks@gmail.com>
2021-02-03 10:16:36 +0800

    🐞fixed bug by redirecting without waiting for user info

--- web/src/utils/hooks/useRecieveUser.ts
+++ web/src/utils/hooks/useRecieveUser.ts
@@ -3,16 +3,18 @@ import { SessionActionType, useSession } from "@components/Session"
 import { useAuthRequest } from "./useAuthRequest"
 
 export const useRecieveUser = () => {
-    const [{token}, dispatch] = useSession()
+    const [{initial, token}, dispatch] = useSession()
     const requestUser = useAuthRequest('/user')
     useEffect(() => {
-        requestUser.call().then(response => {
-            if(!(response instanceof Error) && ["ok", "error"].includes(response.data?.status)) {
-                dispatch({ 
-                    type: SessionActionType.SET_USER,
-                    payload: response.data.user
-                })
-            }
-        })
-    }, [token])
+        if(!initial) {
+            requestUser.call().then(response => {
+                if(!(response instanceof Error) && ["ok", "error"].includes(response.data?.status)) {
+                    dispatch({ 
+                        type: SessionActionType.SET_USER,
+                        payload: response.data.user || null
+                    })
+                }
+            })
+        }
+    }, [token, initial])
 }
