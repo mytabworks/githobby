c59e801428cc24c1e293219ea470c7471d07c705
mytabworks <mytabworks@gmail.com>
2021-02-04 18:34:29 +0800

    🔨 add className props

--- web/src/app/Loader/index.tsx
+++ web/src/app/Loader/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import Spinner from '@components/Spinner'
+import classNames from '@utils/classNames'
 
-const Loader: React.FunctionComponent = () => (
-    <div className="p-absolute top-0 bottom-0 right-0 left-0 d-flex flex-center-x pt-3">
+interface LoaderProps {
+    className?: string;
+}
+
+const Loader: React.FunctionComponent<LoaderProps> = ({ className }) => (
+    <div className={classNames("p-absolute top-0 bottom-0 right-0 left-0 d-flex flex-center-x pt-3", {}, className)}>
         <Spinner/>
     </div>
 )
