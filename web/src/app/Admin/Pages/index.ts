b23819bd4e535fd1f284b41116bb0610b8083242
mytabworks <mytabworks@gmail.com>
2021-02-04 13:52:20 +0800

    ✨ export admin pages

---
+++ web/src/app/Admin/Pages/index.ts
@@ -0,0 +1,8 @@
+import { lazy } from 'react'
+
+export const Login = lazy(() => import('./Login'))
+
+export const Dashboard = lazy(() => import('./Dashboard'))
+
+export const Users = lazy(() => import('./Users'))
+
