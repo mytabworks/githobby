26718247c41fd7f860d1b26ff79ca5db707857ed
mytabworks <mytabworks@gmail.com>
2021-02-04 18:31:08 +0800

    🎨 re-design supense and apply 404 pages

--- web/src/app/Admin/index.tsx
+++ web/src/app/Admin/index.tsx
@@ -7,35 +7,41 @@ import Col from '@components/Layout/Col'
 import Loader from '@app/Loader'
 import Navbar from '@app/Navbar'
 import Sidebar from './Sidebar'
+import { NotFound } from '@app/Pages'
 import { Login, Dashboard, Users } from './Pages'
 
 const Admin: React.FunctionComponent = (props) => {
     const [{user}] = useSession()
     const {path} = useRouteMatch()
-    return (
+    return user ? (
+        <>
+            <Navbar brand="Admin" brandUrl="/admin" filled/>
+            <Container className="main" filled>
+                <Row className="pt-3">
+                    <Col sm={4} md={3} lg={3} xl={1}>
+                        <Sidebar/>
+                    </Col>
+                    <Col sm={8} md={9} lg={9} xl={10}>
+                    <Suspense fallback={<Loader/>}>
+                        <Switch>
+                            <Route exact path={`${path}`} component={Dashboard}/>
+                            <Route exact path={`${path}/users`} component={Users}/>
+                            <Route path="*" component={NotFound}/>
+                        </Switch>
+                    </Suspense>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    ) : (
         <Suspense fallback={<Loader/>}>
-            {user ? (
-                <>
-                    <Navbar brand="Admin" brandUrl="/admin" filled/>
-                    <Container className="main" filled>
-                        <Row className="pt-3">
-                            <Col sm={4} md={3} lg={3} xl={1}>
-                                <Sidebar/>
-                            </Col>
-                            <Col sm={8} md={9} lg={9} xl={10}>
-                            <Switch>
-                                <Route exact path={`${path}`} component={Dashboard}/>
-                                <Route exact path={`${path}/users`} component={Users}/>
-                            </Switch>
-                            </Col>
-                        </Row>
-                    </Container>
-                </>
-            ) : (
+            <Switch>
                 <Route exact path={`${path}`} component={Login}/>
-            )}
+                <Route path="*" component={NotFound}/>
+            </Switch>
         </Suspense>
     )
+    
 }
 
 export default Admin
