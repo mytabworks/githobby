52ed18b21be360942a66d4334f6d5ee099c03760
mytabworks <mytabworks@gmail.com>
2021-02-04 18:31:37 +0800

    ✨ count user integration

--- web/src/app/Admin/Pages/Dashboard/index.tsx
+++ web/src/app/Admin/Pages/Dashboard/index.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Card from '@components/Card'
 import Icon from '@components/Icon'
 import Col from '@components/Layout/Col'
 import Row from '@components/Layout/Row'
 import Text from '@components/Text'
+import HeadGear from '@components/HeadGear'
+import { useAuthRequest } from '@utils/hooks/useAuthRequest'
+import Loader from '@app/Loader'
 
 const Dashboard: React.FunctionComponent = () => {
+    const request = useAuthRequest('/users')
+    useEffect(() => {
+        request.call({
+            params: {
+                filters: "[]"
+            }
+        })
+    }, [])
     return (
         <Row>
+            <HeadGear
+                title="Githobby | Admin - Dashboard"
+                description=""
+                />
             <Col sm={6}>
                 <Card>
                     <Card.Header>
@@ -17,7 +32,10 @@ const Dashboard: React.FunctionComponent = () => {
                         <Text as="h2" align="center" className="t-size-50 my-3">
                             <Icon name="friends"/>
                         </Text>
-                        <Text as="p" align="center" className="t-size-50">10</Text>
+                        <Text as="p" align="center" className="t-size-50">
+                            {request.value?.totalCount || 0}
+                        </Text>
+                        {request.loading && <Loader/>}
                     </Card.Body>
                 </Card>
             </Col>
