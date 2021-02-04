c6d212ac1df8187056a728c263ad6972f031ae38
mytabworks <mytabworks@gmail.com>
2021-02-04 13:40:41 +0800

    ✨ get users with filters

--- server/src/controller/user.ts
+++ server/src/controller/user.ts
@@ -5,23 +5,77 @@ import { UserRole } from '../entity/user'
 
 export default class UserController {
 
-    public static async getUser(_request: Request, response: Response) {
+    public static async getPaginated(request: Request, response: Response) {
+        try {
+            const { sortBy, sortOrder, page = 1 , size = 10, filters } = request.query as any
+            const { payload } = response.locals
+
+            if(!payload) {
+                return response.status(401).send({
+                    status: "error",
+                    message: "unauthorized access"
+                });
+            }
+
+            const user = await User.find(payload.uid)
+
+            if(!user.hasItem || payload.token_version !== user.token_version || !user.roles?.includes(UserRole.ADMIN)) {
+                return response.status(401).send({
+                    status: "error",
+                    message: "unauthorized access"
+                });
+            }
+
+            let userModel = User.select("*", "id as password")
+
+            if(sortBy) {
+                userModel = userModel.orderBy(sortBy, sortOrder)
+            }
+            const parsedFilters = JSON.parse(filters)
+            
+            if(parsedFilters && parsedFilters.length) {
+                userModel = parsedFilters.reduce((userInstance: typeof userModel, filter: any) => {
+                    if(Array.isArray(filter.value)) {
+                        return filter.value.reduce((uI: typeof userModel, value: string) => {
+                            return uI.where(filter.dataIndex, value)
+                        }, userInstance)
+                    }
+                    return userInstance.where(filter.dataIndex, filter.value)
+                }, userModel)
+            }
+
+            const users = await userModel.paginate(parseInt(page), parseInt(size))
+
+            return response.status(200).send({
+                status: "ok",
+                data: users.toJSON(),
+                totalCount: users.totalCount()
+            })
+        } catch(error) {
+            return response.status(500).send({
+                status: "error",
+                message: error.message
+            });
+        }
+    }
+
+    public static async get(_request: Request, response: Response) {
         try {
             const { payload } = response.locals
 
             if(!payload) {
-                return response.status(200).send({
+                return response.status(401).send({
                     status: "error",
-                    message: "invalid token"
+                    message: "unauthorized access"
                 });
             }
 
             const user = await User.find(payload.uid)
 
             if(!user.hasItem || payload.token_version !== user.token_version) {
-                return response.status(200).send({
+                return response.status(401).send({
                     status: "error",
-                    message: "user doesn't exist"
+                    message: "unauthorized access"
                 });
             }
 
@@ -43,23 +97,23 @@ export default class UserController {
         }
     }
 
-    public static async deleteUser(_request: Request, response: Response) {
+    public static async deactivate(_request: Request, response: Response) {
         try {
             const { payload } = response.locals
 
             if(!payload) {
-                return response.status(200).send({
+                return response.status(401).send({
                     status: "error",
-                    message: "invalid token"
+                    message: "unauthorized access"
                 });
             }
 
             const user = await User.find(payload.uid)
 
             if(!user.hasItem || payload.token_version !== user.token_version) {
-                return response.status(200).send({
+                return response.status(401).send({
                     status: "error",
-                    message: "user doesn't exist"
+                    message: "unauthorized access"
                 });
             }
 
@@ -81,18 +135,18 @@ export default class UserController {
             const { payload } = response.locals
 
             if(!payload) {
-                return response.status(200).send({
+                return response.status(401).send({
                     status: "error",
-                    message: "invalid token"
-                });
+                    message: "unauthorized access"
+                })
             }
 
             const user = await User.find(payload.uid, ['*'], 'activity')
 
             if(!user.hasItem || payload.token_version !== user.token_version) {
-                return response.status(200).send({
+                return response.status(401).send({
                     status: "error",
-                    message: "user doesn't exist"
+                    message: "unauthorized access"
                 });
             }
 
