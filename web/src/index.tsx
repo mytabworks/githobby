1c0dfc9785981af42ce818b68626d18b8f24859f
mytabworks <mytabworks@gmail.com>
2021-02-20 11:01:46 +0800

    ✨⚡️ add service worker setup

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './app';
+import * as serviceWorker from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import Session from '@components/Session'
 import '@utils/rulesExtension';
@@ -13,15 +14,39 @@ const defaultSession = {
   user: null
 }
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = document.getElementById('root')
+
+if(root!.hasChildNodes()) {
+  ReactDOM.hydrate(
     <Session defaults={defaultSession}>
       <App />
-    </Session>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+    </Session>,
+  root);
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Session defaults={defaultSession}>
+        <App />
+      </Session>
+    </React.StrictMode>,
+    root);
+}
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+if(process.env.NODE_ENV === 'production') {
+  serviceWorker.register({
+      onUpdate: (registration: any) => {
+          alert('New version available!  Ready to update?');
+          window.location.reload();
+          if (registration && registration.waiting) {
+              registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+          }
+      }
+  });
+} else {
+  serviceWorker.unregister();
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
