8ebd72af7358df622a8fc664cfba81a441cd92b7
mytabworks <mytabworks@gmail.com>
2021-02-20 10:44:11 +0800

    🥅 support item is type user

--- web/src/app/Pages/Profile/Tabs/Other.tsx
+++ web/src/app/Pages/Profile/Tabs/Other.tsx
@@ -4,6 +4,9 @@ import { useGithub } from '@utils/hooks/useGithub'
 import Loader from '@app/Loader'
 import Repository from '@app/Pages/Landing/Repository'
 import HeadGear from '@components/HeadGear'
+import Text from '@components/Text'
+import Row from '@components/Layout/Row'
+import Col from '@components/Layout/Col'
 
 interface OtherProps {
     title: string;
@@ -21,7 +24,20 @@ const Other: React.FunctionComponent<OtherProps> = ({ title, request, fallback }
             {request.value && request.value.length ? (
                     request.value?.map((item: any) => {
                         return (
-                            <Repository key={item.name} item={item} owner />
+                            item.type === 'User' ? (
+                                <Row key={item.id} className="py-4">
+                                    <Col sm={{span: 6, offset: 3}}>
+                                        <img src={item.avatar_url} />
+                                        <div className="repository-title mt-3">
+                                            <a href={item.html_url} target="_blank">
+                                                <Text as="h2">{item.login}</Text>
+                                            </a>
+                                        </div>
+                                    </Col>
+                                </Row>
+                            ) : (
+                                <Repository key={item.id} item={item} owner />
+                            )
                         )
                     })
                 ) : (
